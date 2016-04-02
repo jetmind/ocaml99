8997ed285bc8e97b7f4f3dd04212d1d96bb2de3c
Igor Bondarenko <jetmind2@gmail.com>
2016-04-02 18:16:52 +0300

    Problem 10 done

--- encode.ml
+++ encode.ml
@@ -1,7 +1,17 @@
 (* Run-length encoding of a list *)
 
-let encode l = l;;
+let encode l =
+  let rec collect count acc = function
+    | [] -> acc
+    | [x] -> (count + 1, x) :: acc
+    | a :: (b :: _ as tail) ->
+      if a = b then collect (count + 1) acc tail
+      else collect 0 ((count + 1, a) :: acc) tail in
+  List.rev (collect 0 [] l);;
+
 
 encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
 (* - : (int * string) list =
  * [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")] *)
+
+encode [];;
